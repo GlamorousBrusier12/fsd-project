971838b49296cec51bbdc96ff70f7fa226e4fbcd
Naveen <pandurebelsachin18@gmail.com>
2021-11-13 22:01:34 +0530

    added Store

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">Electorent</div>;
-}
-
-export default App;
---
+++ src/reducers/products.js
@@ -0,0 +1,4 @@
+export default function products(state = []) {
+  //{ products:[]}
+  return state;
+}
