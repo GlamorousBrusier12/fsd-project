eb32edca5dacd4133a4104de86cda257e2b48b16
Naveen <pandurebelsachin18@gmail.com>
2021-12-10 16:17:30 +0530

    updated imports

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import Home from "./Home";
-import ProductPage from "./ProjectPage";
+import ProductPage from "./ProductPage";
 import ReviewForm from "./ReviewForm";
 class App extends Component {
   constructor(props) {
