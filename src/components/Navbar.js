519f91e5cbb20bb691cb874013c2468c56f049ff
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2021-12-13 21:58:43 +0530

    Modified Navabr Styles and Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const styles = {
     marginRight: 30,
   },
   cartCount: {
-    background: "gray",
+    // background: "gray",
     borderRadius: "50%",
     padding: "0px 6px",
     position: "absolute",
-    right: 5,
+    right: 1,
     top: 12,
   },
 };
@@ -42,15 +42,33 @@ class Navbar extends Component {
         <div className="navbar-routes">
           <div className="links" style={styles.cartIconContainer}>
             <Link to="/cart">
-              <i className="fas fa-shopping-cart fa-2x"></i>
-              <span style={styles.cartCount}>5</span>
+              {/* <i className="fas fa-shopping-cart fa-2x"></i> */}
+              <img
+                style={{ height: 30, width: 30 }}
+                src="https://cdn-icons.flaticon.com/png/512/4051/premium/4051933.png?token=exp=1639299642~hmac=cff9f59c57aabbf706a89f689d844980"
+                // src="https://cdn-icons.flaticon.com/png/512/4051/premium/4051778.png?token=exp=1639298511~hmac=6f38febece00451f6577c3501c2e419c"
+                // src="https://cdn-icons.flaticon.com/png/512/4051/premium/4051787.png?token=exp=1639299642~hmac=eaad90cf1b65d72bb50e9828cd3bf786"
+              />
+              <span style={styles.cartCount}>
+                <b>18</b>
+              </span>
             </Link>
           </div>
           <div className="links">
-            <Link to="/wishlist">Wishlist </Link>
+            <Link to="/wishlist">
+              <img
+                style={{ height: 30, width: 30 }}
+                src="https://cdn-icons.flaticon.com/png/128/4051/premium/4051800.png?token=exp=1639298511~hmac=641bf7b760f6c7e21aeb7d36f49417b6"
+              />{" "}
+            </Link>
           </div>
           <div className="links">
-            <Link to="/login">Login/Sign-up</Link>
+            <Link to="/login">
+              <img
+                style={{ height: 30, width: 30 }}
+                src="https://cdn-icons.flaticon.com/png/128/4055/premium/4055362.png?token=exp=1639298511~hmac=6c9bb2e152caa1ffec9de9f890a5dcb0"
+              />
+            </Link>
           </div>
         </div>
       </div>
