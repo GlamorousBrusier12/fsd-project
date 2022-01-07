d3fd6f0c4bf9e077649ecd60f6aac9db135b44a2
Naveen <pandurebelsachin18@gmail.com>
2022-01-07 12:16:35 +0530

    added local qty operations in cart

--- src/components/MiniCart.js
+++ src/components/MiniCart.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
 function MiniCart(props) {
   let newitem = props.content;
+  const [itemQty, setItemQty] = useState(1);
+  useEffect(() => {
+    setItemQty(props.content.qty);
+  }, []);
   const add = () => {
-    newitem.qty = newitem.qty + 1;
+    // newitem.qty = newitem.qty + 1;
+    setItemQty(itemQty + 1);
   };
   const remove = () => {
-    if (newitem.qty >= 1) newitem.qty = newitem.qty - 1;
-    else newitem.qty = 0;
+    let newQty;
+    if (itemQty >= 1) newQty = itemQty - 1;
+    else newQty = 0;
+    setItemQty(newQty);
   };
 
   return (
@@ -19,7 +28,7 @@ function MiniCart(props) {
         <h4>{newitem.title}</h4>
         <h4>₹{newitem.price}</h4>
         <h5>
-          Total:{newitem.qty} x {newitem.price}={newitem.qty * newitem.price}
+          Total:{itemQty} x {newitem.price}={itemQty * newitem.price}
         </h5>
       </div>
 
