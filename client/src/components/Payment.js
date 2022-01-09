2f4865a3aa64260c0ac1691879e777e8f3994608
Chetan Guduru <gchetan2000@gmail.com>
2022-01-09 21:52:29 +0530

    Removed warning signs

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/Payment.css";
 import {connect} from "react-redux"
