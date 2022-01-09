f3a98c58f3b1f6bb7fd8be972c7840e66b236a0c
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2022-01-09 14:48:12 +0530

    Added icons and Buy/Rent filters in products page.

--- src/components/EachProduct.js
+++ src/components/EachProduct.js
@@ -1,18 +1,30 @@
 import React, { useRef } from "react";
 import StarRatings from "react-star-ratings";
 import { Link } from "react-router-dom";
+import { handleaddtoCart } from "../actions/cartAction";
+import { connect } from "react-redux";
+import { toast } from "react-toastify";
 function EachProduct(props) {
   // console.log("each product:", props.content);
   // Getting the data from Products.js as Props
   const { title, image, price, rating, discount, id } = props.content;
-  const iconChange = useRef(null);
-  const onHover = () => {
-    iconChange.current.src =
+  const WishlistIconChange = useRef(null);
+  const CartIconChange = useRef(null);
+  const WonHover = () => {
+    WishlistIconChange.current.src =
       "https://cdn-icons.flaticon.com/png/512/2589/premium/2589054.png?token=exp=1641714832~hmac=4238930e64f5949ff7aed801e590c51b";
   };
-  const onRemove = () => {
-    iconChange.current.src =
+  const WonRemove = () => {
+    WishlistIconChange.current.src =
       "https://cdn-icons-png.flaticon.com/512/535/535285.png";
+  };
+  const ConHover = () => {
+    CartIconChange.current.src =
+      "https://cdn-icons.flaticon.com/png/512/650/premium/650012.png?token=exp=1641719604~hmac=767f91cf73ddcd95068bfe372d98890b";
+  };
+  const ConRemove = () => {
+    CartIconChange.current.src =
+      "https://cdn-icons.flaticon.com/png/512/649/premium/649931.png?token=exp=1641719534~hmac=7df3c20d68a3958384bec50aacf7d793";
   };
   // console.log(props);
   return (
@@ -21,14 +33,29 @@ function EachProduct(props) {
         <p id="wishlistaction">
           <img
             id="beforeadding"
-            ref={iconChange}
-            style={{ height: 20, width: 20 }}
+            ref={WishlistIconChange}
+            style={{ height: 30, width: 30 }}
             src="https://cdn-icons-png.flaticon.com/512/535/535285.png"
             onMouseOver={() => {
-              onHover();
+              WonHover();
             }}
             onMouseOut={() => {
-              onRemove();
+              WonRemove();
+            }}
+          />
+          <img
+            ref={CartIconChange}
+            style={{ height: 30, width: 30 }}
+            src="https://cdn-icons.flaticon.com/png/512/649/premium/649931.png?token=exp=1641719534~hmac=7df3c20d68a3958384bec50aacf7d793"
+            onClick={() => {
+              props.dispatch(handleaddtoCart(id));
+              toast.success("Your Item Added to Cart");
+            }}
+            onMouseOver={() => {
+              ConHover();
+            }}
+            onMouseOut={() => {
+              ConRemove();
             }}
           />
         </p>
@@ -67,4 +94,10 @@ function EachProduct(props) {
   );
 }
 
-export default EachProduct;
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+export default connect(mapStateToProps)(EachProduct);
