5dbc0789c7822cfc7ba2894d3eef416ddd46917b
Naveen <pandurebelsachin18@gmail.com>
2022-01-10 00:05:14 +0530

    After final review(front end)

--- client/src/components/Faqs.js
+++ client/src/components/Faqs.js
@@ -9,17 +9,19 @@ function Faqs(props) {
   const [faqs, setFaqs] = useState(props.items);
   const [input, setInput] = useState("");
 
-  const [pageNumber,setPageNumber] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0);
 
   const usersPerPage = 5;
   const pagesVisited = pageNumber * usersPerPage;
-  const pageCount = Math.ceil(faqs.length/usersPerPage)
-  const changePage = ({selected})=>{
+  const pageCount = Math.ceil(faqs.length / usersPerPage);
+  const changePage = ({ selected }) => {
     setPageNumber(selected);
-  }
-  const displayFaqs = faqs.slice(pagesVisited,pagesVisited+usersPerPage).map((faq)=>{
-    return <Faq content={faq} key={faq.id} />
-  })
+  };
+  const displayFaqs = faqs
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((faq) => {
+      return <Faq content={faq} key={faq.id} />;
+    });
 
   const updateFaqs = (event) => {
     const filtered = faqsDefault.filter((faq) => {
@@ -31,48 +33,47 @@ function Faqs(props) {
     setInput(event.target.value);
     setFaqs(filtered);
   };
-    return (
-      <div className="display-faqs">
-        <h1 className="faqs-heading">Frequent Questions and Answers</h1>
-        <div className="search-area">
-          <input
-            className="faq-search"
-            type="text"
-            placeholder="Search.."
-            name="search"
-            onChange={updateFaqs}
-            value={input}
-          />
-          <button className="faq-search-button">
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-        {/* <input className="faqs-search"/> */}
-        <div className="faqs">
-          {/* {faqs.map((item) => (
+  return (
+    <div className="display-faqs">
+      <h1 className="faqs-heading">Frequent Questions and Answers</h1>
+      <div className="search-area">
+        <input
+          className="faq-search"
+          type="text"
+          placeholder="Search.."
+          name="search"
+          onChange={updateFaqs}
+          value={input}
+        />
+        <button className="faq-search-button">
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+      {/* <input className="faqs-search"/> */}
+      <div className="faqs">
+        {/* {faqs.map((item) => (
             <Faq content={item} key={item.id} />
           ))} */}
-          {displayFaqs}
-          <ReactPaginate
-            previousLabel={"Previous"}
-            nextLabel={"Next"}
-            pageCount={pageCount}
-            onPageChange={changePage}
-            containerClassName={"paginationBtns"}
-            previousLinkClassName={"previousBtn"}
-            nextLinkClassName={"nextBtn"}
-            disabledClassName={"paginationDisabled"}
-            activeClassName={"paginationActive"}
-          />
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-
-        </div>
+        {displayFaqs}
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBtns"}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Faqs;
