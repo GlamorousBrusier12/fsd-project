648f1597d87129b92d71d457077ae45d7ac589e6
Ravindra Tholuchuru <tholuchururavindra@gmail.com>
2022-01-09 13:25:18 +0530

    Added wishlist icon in products page.

--- src/components/EachProduct.js
+++ src/components/EachProduct.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import StarRatings from "react-star-ratings";
 import { Link } from "react-router-dom";
 function EachProduct(props) {
   // console.log("each product:", props.content);
   // Getting the data from Products.js as Props
   const { title, image, price, rating, discount, id } = props.content;
+  const iconChange = useRef(null);
+  const onHover = () => {
+    iconChange.current.src =
+      "https://cdn-icons.flaticon.com/png/512/2589/premium/2589054.png?token=exp=1641714832~hmac=4238930e64f5949ff7aed801e590c51b";
+  };
+  const onRemove = () => {
+    iconChange.current.src =
+      "https://cdn-icons-png.flaticon.com/512/535/535285.png";
+  };
   // console.log(props);
   return (
     <div>
       <div className="each-product">
+        <p id="wishlistaction">
+          <img
+            id="beforeadding"
+            ref={iconChange}
+            style={{ height: 20, width: 20 }}
+            src="https://cdn-icons-png.flaticon.com/512/535/535285.png"
+            onMouseOver={() => {
+              onHover();
+            }}
+            onMouseOut={() => {
+              onRemove();
+            }}
+          />
+        </p>
         <div>
           <Link to={`/products/${id}`}>
             <div className="img-size">
