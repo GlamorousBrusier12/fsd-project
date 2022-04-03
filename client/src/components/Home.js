ac7ff7955ec8aa2ca33339f8ecc14171273f3671
Naveen <pandurebelsachin18@gmail.com>
2022-04-03 13:10:48 +0530

    resolved home page errors

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import MiniProduct from "./MiniProduct";
 import Slider from "./Slider";
 import "../styles/home.css";
 import Categories from "./Categories";
 import { getAllProducts } from "../utils/api";
-class Home extends Component {
-  constructor() {
-    super();
-    // initial state{ deals:[] }
-    this.state = {
-      deals: [],
-    };
-  }
-  componentDidMount() {
+const Home = () => {
+  // constructor() {
+  //   super();
+  //   // initial state{ deals:[] }
+  //   this.state = {
+  //     deals: [],
+  //   };
+  // }
+  const [deals, setDeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
     Promise.resolve(
       getAllProducts().then(
         (result) => {
           // set products as deals
-          this.setState({ deals: result });
-          console.log(this.state);
-          console.log(result);
+          setDeals(result.data);
+          console.log(deals);
+          setLoading(false);
         },
         (error) => {
           console.log(
@@ -29,45 +32,51 @@ class Home extends Component {
         }
       )
     );
-  }
-  render() {
-    let { deals } = this.state;
-    deals = deals.slice(0, 8);
+  }, []);
 
-    return (
-      <div>
-        <Categories />
-        <Slider />
-        <div className="deals-container">
-          <h3>Deals of the day!!!</h3>
-          <div className="mini-products">
-            {deals.map((product) => (
-              <MiniProduct data={product} key={product.id} />
-            ))}
-          </div>
-        </div>
-        <div className="deals-container">
-          <h3>Rents of the day!!!</h3>
-          <div className="mini-products">
-            {deals.map((product) => (
-              <MiniProduct data={product} key={product.id} pid={product.id} />
-            ))}
-          </div>
-        </div>
-        <div className="deals-container">
-          <h3>
-            continue shopping
-            <i className="fas fa-arrow-right" style={{ marginLeft: 5 }}></i>
-          </h3>
-          <div className="mini-products">
-            {deals.map((product, index) => (
-              <MiniProduct data={product} key={product.id} />
-            ))}
-          </div>
+  // componentDidMount() {
+
+  // }
+  // render() {
+  // let { deals } = this.state;
+  // deals = deals.slice(0, 8);
+
+  return loading === true ? (
+    <p>loading...</p>
+  ) : (
+    <div>
+      <Categories />
+      <Slider />
+      <div className="deals-container">
+        <h3>Deals of the day!!!</h3>
+        <div className="mini-products">
+          {deals.map((product) => (
+            <MiniProduct data={product} key={product.id} />
+          ))}
         </div>
       </div>
-    );
-  }
-}
+      <div className="deals-container">
+        <h3>Rents of the day!!!</h3>
+        <div className="mini-products">
+          {deals.map((product) => (
+            <MiniProduct data={product} key={product.id} pid={product.id} />
+          ))}
+        </div>
+      </div>
+      <div className="deals-container">
+        <h3>
+          continue shopping
+          <i className="fas fa-arrow-right" style={{ marginLeft: 5 }}></i>
+        </h3>
+        <div className="mini-products">
+          {deals.map((product, index) => (
+            <MiniProduct data={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+  // }
+};
 
 export default Home;
