6cbff26df6c206345118933c8cbe5ddd85ab5ca0
k-bhargav <saibhargav246@gmail.com>
2022-04-04 10:40:11 +0530

    Completed Myorders page

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -2,13 +2,9 @@ import "../styles/myprofile.css";
 import Sidebar from "./Sidebar";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link, Redirect } from "react-router-dom";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { toast } from "react-toastify";
-import { toastStyler } from "../commonEquipment";
-import { handleUser } from "../actions";
-import { getMyOrders, getOneProduct } from "../utils/api";
+import { getMyOrders } from "../utils/api";
 
 //This page renders all Myorders
 const UserProfile = (props) => {
@@ -18,51 +14,19 @@ const UserProfile = (props) => {
   //We set the existing data.
   useEffect(() => {
     getMyOrders(props.user._id).then((res) => {
-      console.log(res.data.userOrders);
       let newArray = res.data.userOrders;
       newArray.forEach((address) => {
         address.id = address._id.toString();
+        address.type = address.productId.type;
+        address.category = address.productId.category;
+        address.discount = address.productId.discount;
+        address.price = address.productId.price;
       });
       console.log(newArray);
       setData(newArray);
     });
   }, [props.user.myOrders]);
 
-  /* getOneProduct(props.user.productId).then();
-  console.log("product is", product); */
-  /* const handleDelete = (id) => {
-    //We delete this particular id.
-    let afterDelete = data.filter((item) => item.id !== id);
-
-    //Set the new array
-    setData(afterDelete);
-
-    //We patch the entire user.
-    newUser.myOrders = afterDelete;
-    let url = "http://localhost:3000/users/" + props.user.id;
-
-    const token = localStorage.getItem("JWTToken");
-    fetch(url, {
-      method: "PATCH", // or 'PUT'
-      headers: {
-        Accept: "application/json",
-        Authorization: "Bearer ",
-        // Authorization: Bearer ${},
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Successfully PATCHED", data);
-        props.dispatch(handleUser(props.user._id));
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-    toast.warning("Order Deleted", toastStyler);
-  };
- */
   //Column headings
   const columns = [
     {
@@ -90,22 +54,22 @@ const UserProfile = (props) => {
     },
 
     {
-      field: "productId.type",
+      field: "type",
       headerName: "Type",
       width: 80,
     },
     {
-      field: "productId.category",
+      field: "category",
       headerName: "Category",
       width: 150,
     },
     {
-      field: "productId.discount",
+      field: "discount",
       headerName: "Discount",
       width: 100,
     },
     {
-      field: "productId.price",
+      field: "price",
       headerName: "Price",
       width: 100,
     },
@@ -130,10 +94,6 @@ const UserProfile = (props) => {
             >
               <button className="userListEdit">Post Review</button>
             </Link>
-            {/* <DeleteOutlineIcon
-              className="userListDelete"
-              onClick={() => handleDelete(params.row.id)}
-            /> */}
           </>
         );
       },
